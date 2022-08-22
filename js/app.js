82699306bc674024207f623fd1655e62ebeb685e
sadiq1020 <sajidnsubd@gmail.comm>
2022-08-22 20:48:16 +0600

    updated validation

--- js/app.js
+++ js/app.js
@@ -1,44 +1,77 @@
+const playersArray = [];
 
-// // document.getElementById('.addEventListener').addEventListener
 
-// // document.getElementsByClassName('btn-player').addEventListener('click', function () {
-// //     console.log('clicked');
-// // })
 
-// const playerButtons = document.getElementsByClassName('btn-player');
-// for (const playerButton of playerButtons) {
-//     playerButton.addEventListener('click', function () {
-//         // console.log('clicked');
-
-//         const playerNames = document.getElementsByClassName('player-name')
-//         const playerName = playerNames[0];
-//         console.log(playerName.innerText);
-//         // for (const playerName of playerNames) {
-//         //     // console.log(playerName);
-//         //     const addedPlayer = playerName.innerText;
-//         //     console.log(addedPlayer);
-
-//         //     // const selectedPlayerField = document.getElementById('selected-players');
-//         //     // const p = document.createElement('p');
-//         //     // p.innerText = addedPlayer;
-//         //     // selectedPlayerField.appendChild(p);
-//         // }
-//     })
+// function playerNameList(fiveDone) {
+//     if (fiveDone === true) {
+//         alert('You can not buy more than 5 players!');
+//         return true;
+//     }
 // }
 
-const playersArray = [];
-// console.log(playersArray.length);
-
 function addToPlayerList(element) {
+
+    element.setAttribute('disabled', true);
     // console.log(element.parentNode.parentNode.children[0].innerText);
     const playerName = element.parentNode.parentNode.children[0].innerText;
 
     const selectedPlayerField = document.getElementById('selected-players');
     const li = document.createElement('li');
     li.innerText = playerName;
-    selectedPlayerField.appendChild(li);
-    playersArray.push(li.innerText);
+
+    const selectedPlayers = document.querySelector('#selected-players');
+
+    if (selectedPlayers.children.length <= 4) {
+        selectedPlayerField.appendChild(li);
+    }
+    else {
+        alert('asdasd');
+        return;
+    }
+
+
+
+
+    // playersArray.push(li.innerText);
+
+    // if (playersArray.length < 6) {
+    //     // playersArray.push(li.innerText);
+    // }
+    // else {
+    //     alert('asdasdasdsad');
+    //     return;
+    // }
+
+    // const selectedPlayers = document.querySelector('#selected-players');
+    // console.log(selectedPlayers.children.length);
+
+    // if (selectedPlayers.children.length > 4) {
+    //     alert('asdasd');
+    //     return;
+    // }
+
+    // const playersList = document.querySelectorAll('#selected-players')
+    // const numberOfPlayers = playersList.children.children;
+    // console.log(numberOfPlayers.length);
+    // // if (playersList.length > 5) {
+    // //     alert('adsas');
+    // // }
+
+
+    // if (document.querySelectorAll('#selected-players li').length > 4) {
+    //     alert('haha');
+    // }
+    // return;
+
+    // if (playersArray.length === 5) {
+    //     alert('You can not buy more than 5 players!');
+    //     return;
+    // }
+    // playerNameList(playersArray.length === 5);
+
 }
+
+
 
 function getInputFieldValueById(inputFieldId) {
     const inputField = document.getElementById(inputFieldId);
@@ -54,7 +87,10 @@ function setTextElementvalueById(elementId, newValue) {
 
 document.getElementById('btn-calculate').addEventListener('click', function () {
     const playerBudget = getInputFieldValueById('player-budget-field')
-    const totalPlayerExpense = playerBudget * playersArray.length;
+
+    const selectedPlayers = document.querySelector('#selected-players');
+
+    const totalPlayerExpense = playerBudget * selectedPlayers.children.length;
     // console.log(totalPlayerExpense);
     setTextElementvalueById('player-expenses', totalPlayerExpense);
 })
